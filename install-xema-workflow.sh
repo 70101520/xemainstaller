c2a04e92aea7c4e2565f2e44acf1c1c3ca6cb122
70101520 <84832845+70101520@users.noreply.github.com>
2026-07-11 13:14:56 +0530

    Set PJSIP transport binds in workflow installer

--- install-xema-workflow.sh
+++ install-xema-workflow.sh
@@ -3,7 +3,7 @@ set -euo pipefail
 
 OFFICIAL_INSTALL_URL="${XEMA_BASE_INSTALL_URL:-https://raw.githubusercontent.com/xema-in/install/master/install-xema.sh}"
 PACKAGE_URL="${XEMA_WORKFLOW_PACKAGE_URL:-https://raw.githubusercontent.com/70101520/xemainstaller/main/packages/xema-workflow-linux-x64.tgz}"
-PACKAGE_SHA256="${XEMA_WORKFLOW_PACKAGE_SHA256:-256d0fa2e7ff374ab63bd90c7add06dccc4d7c743bf40ed63d663e65c1efab22}"
+PACKAGE_SHA256="${XEMA_WORKFLOW_PACKAGE_SHA256:-fcd84a9d5f40695b8424564a09201708ca95635203abe34bea6c10f0725ac0ad}"
 
 SKIP_BASE=0
 SKIP_UPGRADE=0
